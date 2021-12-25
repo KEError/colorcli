97b7bbcc4308a49528ae3f3f07085a2a70450b18
durgeshrawat <66310896+durgeshrawat@users.noreply.github.com>
2021-12-25 19:51:57 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,33 @@
+#color set
+def set(text,color,bg='black',style='normal'):
+    _colorDirectory={
+        'black':'30',
+        'red':'31',
+        'green':'32',
+        'yellow':'33',
+        'cyan':'36',
+        'purple':'35',
+        'blue':'34',
+        'white':'37'
+    }
+    _style={
+        'bold':'1',
+        'normal':'0',
+        'italic':'2'
+    }
+    _change='\033['+str(_style[style.lower()])+';'+str(_colorDirectory[color.lower()])+';'+str(int(_colorDirectory[bg.lower()])+10)+'m'
+    _restore='\033['+str(_style['normal'])+';'+str(_colorDirectory['white'])+';'+str(int(30+10))+'m'
+    try:
+        return _change+str(text)+_restore
+    except:
+        return text
+        
+if True:
+    print(set('black','black',bg='red'))
+    print(set('red   ','red'))
+    print(set('green ','green'))
+    print(set('yellow','yellow'))
+    print(set('blue  ','blue'))
+    print(set('purple','purple'))
+    print(set('cyan ','cyan'))
+    print(set('white','white'))
---
+++ install.py
@@ -0,0 +1,7 @@
+from os import system
+from sys import path
+from color import set
+print(set('installing','white',style='bold'),set('colorcli','white'),'please wait ..')
+p=path
+#system('cp color.py '+p[-1])
+print(set('installed sucessfully ..','green',style='bold'))
