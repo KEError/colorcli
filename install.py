c66938252749d3686ae792b066e1014716988f99
durgeshrawat <66310896+durgeshrawat@users.noreply.github.com>
2021-12-26 13:17:54 +0530

    Update install.py

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ from sys import path
 from color import set
 print(set('installing','white',style='bold'),set('colorcli','white'),'please wait ..')
 p=path
-#system('cp color.py '+p[-1])
+system('cp color.py '+p[-1])
 print(set('installed sucessfully ..','green',style='bold'))
